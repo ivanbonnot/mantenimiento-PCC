646c049eb7f7d9beeb9178cfb79de4da05a5fed6
Ivan Bonnot <ivanbonnot@gmail.com>
2023-08-15 13:25:07 -0300

    primer commit

---
+++ src/constants/stations.js
@@ -0,0 +1,163 @@
+const et = [
+  {
+    title: "Basso",
+    id: 1
+  },
+  {
+    title: "Chajarí",
+    id: 2
+  },
+  {
+    title: "Colón",
+    id: 3
+  },
+  {
+    title: "Colonia Elia",
+    id: 4
+  },
+  {
+    title: "Concordia",
+    id: 5
+  },
+  {
+    title: "Gualeguaychú",
+    id: 6
+  },
+  {
+    title: "Masisa",
+    id: 7
+  },
+  {
+    title: "San José",
+    id: 8
+  },
+  {
+    title: "Uruguay Norte",
+    id: 9
+  },
+  {
+    title: "Uruguay Sur",
+    id: 10
+  },
+  {
+    title: "San Salvador",
+    id: 11
+  },
+  {
+    title: "Villa Elisa",
+    id: 12
+  },
+];
+
+const set = [
+    {
+        title: "Baylina",
+        id: 1
+      },
+      {
+        title: "Bonn",
+        id: 2
+      },
+      {
+        title: "Caseros",
+        id: 3
+      },
+      {
+        title: "Ceibas",
+        id: 4
+      },
+      {
+        title: "Colón",
+        id: 5
+      },
+      {
+        title: "Colonia Oficial",
+        id: 6
+      },
+      {
+        title: "Colonia Roca",
+        id: 7
+      },
+      {
+        title: "El Puente",
+        id: 8
+      },
+      {
+        title: "Federación",
+        id: 9
+      },{
+        title: "General Campos",
+        id: 10
+      },{
+        title: "Humaitá",
+        id: 11
+      },{
+        title: "Ibicuicito",
+        id: 12
+      },{
+        title: "Ibicuy",
+        id: 13
+      },{
+        title: "Juan Jorge",
+        id: 14
+      },{
+        title: "Jubileo",
+        id: 15
+      },{
+        title: "Larroque",
+        id: 16
+      },{
+        title: "Mabragaña",
+        id: 17
+      },
+      {
+        title: "Móvil 5MVA",
+        id: 18
+      },
+      {
+        title: "Nueva Escocia",
+        id: 19
+      },
+      {
+        title: "Paranacito",
+        id: 20
+      },
+      {
+        title: "Pronunciamiento",
+        id: 21
+      },
+      {
+        title: "Puente Quebracho",
+        id: 22
+      },
+      {
+        title: "Redomón",
+        id: 23
+      },{
+        title: "Rocamora",
+        id: 24
+      },{
+        title: "Rosario del Tala",
+        id: 25
+      },{
+        title: "San Marcial",
+        id: 26
+      },{
+        title: "San Vicente",
+        id: 27
+      },{
+        title: "Santa Anita",
+        id: 28
+      },{
+        title: "Urdinarrain",
+        id: 29
+      },{
+        title: "Villa Elisa",
+        id: 30
+      },{
+        title: "Zenón Roca",
+        id: 31
+      }
+]
+
+export default { et, set }
