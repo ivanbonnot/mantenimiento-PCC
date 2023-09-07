09351eab1cfd168d3817c321b79da536a6fbc188
ivanbonnot <ivanbonnot@gmail.com>
2023-09-07 19:49:21 -0300

    armando notas

--- src/container/Notes/Notes.jsx
+++ src/container/Notes/Notes.jsx
@@ -32,16 +32,16 @@ const Notes = () => {
                 <p className="note p__opensans">Aclaración:  {nota}</p>
                 <p className="author p__opensans">Fecha:  {fecha}</p>
                 <p className="date p__opensans">Autor:  {creador}</p>
+                <button type="button" className="delete__button">Eliminar Nota</button>
               </div>
             )}
           </div>
 
           <div className="app__notes-write">
-
+            <h2 className="p__cormorant">Agregar una nota</h2>
             <input type="text" placeholder="Titulo" className="app__notes-write_title" />
-            <textarea type="textarea" row={10} placeholder="Aclaración" className="app__notes-write_note" />
+            <textarea type="textarea" rows={7} placeholder="Aclaración" className="app__notes-write_note" />
             <button type="button" className="custom__button">Agregar nota</button>
-
           </div>
         </div>
       ) : (
